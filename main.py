9bf80305e4da66378ac53e900049ae4ea63206b6
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-10-02 19:53:37 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#4.12 – Mais laços: Todas as versões de foods.py nesta seção evitaram usar
+#laços for para fazer exibições a fim de economizar espaço. Escolha uma
+#versão de foods.py e escreva dois laços for para exibir cada lista de comidas
+Pizza = ['Chocolate', 'carne de sol', 'calabresa']
+churrasco = ['Linguiça', 'maminha', 'coração']
+
+print("Pizzas:")
+for pizza in Pizza:print(pizza)
+
+print("\nChurrascos:")
+for carne in churrasco:print(carne)
